0607e8b6776d4919cabba13c3b08c4e47720a01d
Alicia Wasik <aliciaawasik@lewisu.edu>
2024-10-30 10:27:04 -0700

    update

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,5 +1,12 @@
-import subprocess
+import os
 
-result = subprocess.run(['ls'], capture_output=True, text=True)
+def executeCommand(cmd):
+    print(cmd)
+    os.system(cmd)
+    print("")
 
 print("Hello World!")
+executeCommand("git status")
+# executeCommand("git add -A")
+# executeCommand("git commit -m 'Update files.'")
+# execiteCommand("git push")

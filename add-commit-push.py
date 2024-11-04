86bcae349d6e2199795e99ccc739f504ef1cf050
Alicia Wasik <aliciaawasik@lewisu.edu>
2024-11-03 19:42:14 -0800

    Update code.

--- add-commit-push.py
+++ add-commit-push.py
@@ -11,17 +11,10 @@ print("")
 executeCommand("git status")
 
 response=input("Would you like to AddCommitPush (y/n)? ")
-print(response)
 
 # Check if the input is not equal to "y"
 if response != "y":
-    print("You did not choose 'y'.")
-else:
-    print("You chose 'y'.")
-
-
-sys.exit()
-
+    sys.exit()
 
 executeCommand("git add -A")
 executeCommand("git commit -m \"Update code.\"")
